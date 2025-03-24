7cddfb33cadd3175c0147e6112ef19b447938b22
Lciop-f <18729748561@163.com>
2025-03-24 14:51:02 +0800

    Update release.py

--- apps/release.py
+++ apps/release.py
@@ -2,15 +2,28 @@ import os
 import glob
 
 
-def delete_mp4_files(directory="."):
-    mp4_files = glob.glob(os.path.join(directory, '*.mp4'))
+def delete_v_files(directory="."):
+    webm_files = glob.glob(os.path.join(directory, '*.webm'))
 
-    for file_path in mp4_files:
+    for file_path in webm_files:
+        try:
+            os.remove(file_path)
+            print(f"已删除文件: {file_path}")
+        except Exception as e:
+            print(f"删除文件 {file_path} 时出错: {e}")
+def delete_zip_files(directory="."):
+    zip_files = glob.glob(os.path.join(directory, '*.zip'))
+
+    for file_path in zip_files:
         try:
             os.remove(file_path)
             print(f"已删除文件: {file_path}")
         except Exception as e:
             print(f"删除文件 {file_path} 时出错: {e}")
 
+def delete_temp_files():
+    delete_v_files()
+    delete_zip_files()
+
 if __name__ == '__main__':
-    delete_mp4_files()
+    delete_temp_files()
